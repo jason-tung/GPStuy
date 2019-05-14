a302bf5e1911a070076e9216065922180ec472e7
ivzhang1 <izhang1@stuy.edu>
2019-05-14 13:01:30 -0400

    reorganize mapsolver.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os, csv, time, sqlite3, json
 from random import shuffle
-
+from util import mapsolver
 
 from urllib.request import Request, urlopen
 
@@ -12,43 +12,8 @@ app.secret_key = os.urandom(32)  # key for session
 
 @app.route('/')
 def hello_world():
-    print(getmap(1))
+    return render_template("home.html")
 
-def getmap(x):
-    with open('maps/floor{0}'.format(x), 'r') as file:
-        map_dict = {}
-        map_dict["map"] = file.read()
-        line_ary = map_dict["map"].split("\n")
-        map_dict["stairs"]=line_ary[0]
-        return map_dict
-
-def solvemap(asciimap,loc1,loc2):
-    print(asciimap)
-    print("----")
-    asciimap.replace(loc1,"S").replace(loc2,"E")
-    asciimap = "".join(['#' if k.islower() else k for k in asciimap])
-    asciimap_rows = asciimap.split('\n')
-    mapary = [[k for k in j] for j in asciimap_rows]
-    print(mapary)
-
-def path(loc1,loc2):
-    floor1=loc1[0]
-    floor2=loc2[0]
-    coord1=loc1[1:]
-    coord2=loc2[1:]
-    if floor1 == floor2:
-        getmap(floor1)
-    else:
-        return
-
-with open('maps/floor{0}'.format(1), 'r') as file:
-    map_dict = {}
-    map_dict["map"] = file.read()
-    line_ary = map_dict["map"].split("\n")
-    map_dict["stairs"]=line_ary[0]
-    map_dict["map"] = "".join([k + "\n" for k in line_ary[1:]])[:-1]
-    solvemap(map_dict["map"],'a','c')
-    
 if __name__ == "__main__":
     app.debug = True
     app.run()
