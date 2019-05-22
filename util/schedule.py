be9e3259e8b833c924685693397366f5545ad65b
ivzhang1 <izhang1@stuy.edu>
2019-05-22 09:22:47 -0400

    add num_periods which initializes REGULAR, CONFERENCE, HOMEROOM lists to num periods

--- util/schedule.py
+++ util/schedule.py
@@ -3,10 +3,20 @@ bell_schedule = "" # can be REGULAR, CONFERENCE, HOMEROOM
 type_of_day = "" # can be A, B
 day_of_week = "" #can be MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 days = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
+REGULAR = []
+CONFERENCE = []
+HOMEROOM = []
 
 def return_day(num):
     '''Given a num from 0-6 returns Monday-Sunday respectively'''
     return days[num]
 
-for i in range(7):
-    print(return_day(i))
+def num_periods(num):
+    '''Sets globals REGULAR, CONFERENCE, HOMEROOM to be of length num'''
+    global REGULAR, CONFERENCE, HOMEROOM
+    REGULAR = [None for i in range(num)]
+    CONFERENCE = [None for i in range(num)]
+    HOMEROOM = [None for i in range(num)]
+
+num_periods(10)
+print(REGULAR, CONFERENCE, HOMEROOM)
