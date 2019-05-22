bd93a3b82b26aec19bc87b458f56491508d5ea64
ivzhang1 <izhang1@stuy.edu>
2019-05-22 11:32:12 -0400

    add call to set REGULAR, HOMEROOM, CONFERENCE to Stuy

--- util/schedule.py
+++ util/schedule.py
@@ -11,12 +11,15 @@ def return_day(num):
     '''Given a num from 0-6 returns Monday-Sunday respectively'''
     return days[num]
 
-def num_periods(num):
+def num_periods(num, type):
     '''Sets globals REGULAR, CONFERENCE, HOMEROOM to be of length num'''
     global REGULAR, CONFERENCE, HOMEROOM
-    REGULAR = [None for i in range(num)]
-    CONFERENCE = [None for i in range(num)]
-    HOMEROOM = [None for i in range(num)]
+    if type == "REGULAR":
+        REGULAR = [None for i in range(num)]
+    elif type == "CONFERENCE":
+        CONFERENCE = [None for i in range(num)]
+    elif type == "HOMEROOM":
+        HOMEROOM = [None for i in range(num)]
 
 def set_period(period, start_time, end_time, type):
     '''Sets period in globals REGULAR, CONFERENCE, HOMEROOM (based on type) based on start_time, end_time, walking_time'''
@@ -36,17 +39,13 @@ def set_periods(num_period, start_time, period_length, walking_time):
     start_time = datetime.strptime(start_time, time_format)
     time_delta_period = timedelta(minutes = period_length)
     time_delta_walk = timedelta(minutes = walking_time)
-    num_periods(num_period)
-    if period_length == 41:
-        type = "REGULAR"
-        for i in range(num_period):
-            set_period(i, start_time, start_time + time_delta_period, type)
-            start_time += time_delta_period + time_delta_walk
 
     if period_length == 40:
-        num_periods(num_period+1)
+        num_period += 1 # Accounts for addition 1 homeroom period
         type = "HOMEROOM"
-        for i in range(num_period+1):
+        num_periods(num_period, type)
+
+        for i in range(num_period):
             if i == 3:
                 homeroom_time_delta = timedelta(minutes = 12) # Homeroom is 12 minutes long
                 set_period(i, start_time, start_time + homeroom_time_delta, type)
@@ -55,6 +54,24 @@ def set_periods(num_period, start_time, period_length, walking_time):
             set_period(i, start_time, start_time + time_delta_period, type)
             start_time += time_delta_period + time_delta_walk
 
+    elif period_length == 37:
+        type = "CONFERENCE"
+        num_periods(num_period, type)
+        time_delta_walk = timedelta(minutes = 4)
+
+        for i in range(num_period):
+            set_period(i, start_time, start_time + time_delta_period, type)
+            start_time += time_delta_period + time_delta_walk
+
+    else:
+        type = "REGULAR"
+        num_periods(num_period, type)
+
+        for i in range(num_period):
+            set_period(i, start_time, start_time + time_delta_period, type)
+            start_time += time_delta_period + time_delta_walk
+
+
 set_periods(10, "8:00", 41, 5) # Sets Stuy Regular Schedule
 set_periods(10, "8:00", 40, 5) # Sets Stuy Regular Schedule
-print(HOMEROOM)
+set_periods(10, "8:00", 37, 5) # Sets Stuy Regular Schedule
