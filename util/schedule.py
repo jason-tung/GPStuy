293c13d9d43d21828711efd7222308a50c2800dd
ivzhang1 <izhang1@stuy.edu>
2019-05-22 09:19:20 -0400

    add return_day() which return Weekday given int from 0-6

--- util/schedule.py
+++ util/schedule.py
@@ -1,3 +1,12 @@
 from datetime import datetime
+bell_schedule = "" # can be REGULAR, CONFERENCE, HOMEROOM
+type_of_day = "" # can be A, B
+day_of_week = "" #can be MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+days = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
 
-print(datetime.today())
+def return_day(num):
+    '''Given a num from 0-6 returns Monday-Sunday respectively'''
+    return days[num]
+
+for i in range(7):
+    print(return_day(i))
