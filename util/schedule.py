fb075fbd925e10b9fc0f1e3239103dd399560ec1
ivzhang1 <izhang1@stuy.edu>
2019-05-22 10:54:29 -0400

    add set_period() that sets each individual period start, end times

--- util/schedule.py
+++ util/schedule.py
@@ -18,5 +18,14 @@ def num_periods(num):
     CONFERENCE = [None for i in range(num)]
     HOMEROOM = [None for i in range(num)]
 
-num_periods(10)
-print(REGULAR, CONFERENCE, HOMEROOM)
+def set_period(period, start_time, end_time, walking_time, type):
+    '''Sets period in globals REGULAR, CONFERENCE, HOMEROOM (based on type) based on start_time, end_time, walking_time'''
+    global REGULAR, CONFERENCE, HOMEROOM
+    time_format = "%H:%M"
+    type = type.upper()
+    if type == "REGULAR":
+        REGULAR[period] = (datetime.strptime(start_time, time_format), datetime.strptime(end_time, time_format))
+    elif type == "CONFERENCE":
+        CONFERENCE[period] = (datetime.strptime(start_time, time_format), datetime.strptime(end_time, time_format))
+    elif type == 'HOMEROOM':
+        HOMEROOM[period] = (datetime.strptime(start_time, time_format), datetime.strptime(end_time, time_format))
