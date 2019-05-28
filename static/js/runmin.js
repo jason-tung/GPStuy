246cc03b7f349165c4dfc0af1e91936ba7ff4e76
Jason Tung <mickeu420@gmail.com>
2019-05-28 12:55:32 -0400

    made maps smaler

--- static/js/runmin.js
+++ static/js/runmin.js
@@ -7,12 +7,6 @@
 //   });
 
 var bodstr = `
-    <pre id="test_div" style="font-family: monospace;">
-</pre>
-    <br>-----<br>
-    <pre id="test_div2" style="font-family: monospace;">
-
-</pre>
     <svg id="vimage1" height="500" width="500" style="border: 1px solid;"></svg>
     <br>
     <svg id="vimage2" height="500" width="500" style="border: 1px solid;"></svg>
@@ -39,17 +33,17 @@ sbtn.addEventListener('click', function () {
 
 function do_stuff(maze, pic) {
     var dim1 = [maze.length, maze[0].length];
-    pic.setAttribute("height", dim1[0] * 20);
-    pic.setAttribute("width", dim1[1] * 20);
+    pic.setAttribute("height", dim1[0] * 10);
+    pic.setAttribute("width", dim1[1] * 10);
     for (var i = 0; i < maze.length; i++) {
         for (var j = 0; j < maze[0].length; j++) {
             var c = document.createElementNS("http://www.w3.org/2000/svg", "rect");
             // console.log(c);
-            c.setAttribute("height", 20);
-            c.setAttribute("width", 20);
-            c.setAttribute("rx", 5);
-            c.setAttribute("x", 20 * j);
-            c.setAttribute("y", 20 * i);
+            c.setAttribute("height", 10);
+            c.setAttribute("width", 10);
+            c.setAttribute("rx", 3);
+            c.setAttribute("x", 10 * j);
+            c.setAttribute("y", 10 * i);
             c.setAttribute("stroke", "black");
             // console.log(maze[i][j]);
             // if (!maze[i][j]){
@@ -85,8 +79,8 @@ function do_stuff(maze, pic) {
 function find(a,b) {
     var pic1 = document.getElementById("vimage1");
         var pic2 = document.getElementById("vimage2");
-        var divvy = document.getElementById("test_div");
-        var divvy2 = document.getElementById("test_div2");
+        // var divvy = document.getElementById("test_div");
+        // var divvy2 = document.getElementById("test_div2");
     var promise = new Promise(function (resolve, reject) {
         $.get("/api_path/", {'loc1': a, 'loc2': b})
             .done(function (response) {
@@ -105,21 +99,21 @@ function find(a,b) {
             console.log(maze);
             console.log(floor1);
             console.log(floor2);
-            for (var i = 0; i < floor1.length; i++) {
-                for (var j = 0; j < floor1[0].length; j++) {
-                    divvy.innerHTML += floor1[i][j];
-                }
-                // console.log(divvy)
-                // divvy.innerHTML+=floor1[i];
-                divvy.innerHTML += "<br>";
-            }
-            for (var i = 0; i < floor2.length; i++) {
-                for (var j = 0; j < floor2[0].length; j++) {
-                    divvy2.innerHTML += floor2[i][j];
-                }
-                // divvy.innerHTML+=floor2[i];
-                divvy2.innerHTML += "<br>";
-            }
+            // for (var i = 0; i < floor1.length; i++) {
+            //     for (var j = 0; j < floor1[0].length; j++) {
+            //         divvy.innerHTML += floor1[i][j];
+            //     }
+            //     // console.log(divvy)
+            //     // divvy.innerHTML+=floor1[i];
+            //     divvy.innerHTML += "<br>";
+            // }
+            // for (var i = 0; i < floor2.length; i++) {
+            //     for (var j = 0; j < floor2[0].length; j++) {
+            //         divvy2.innerHTML += floor2[i][j];
+            //     }
+            //     // divvy.innerHTML+=floor2[i];
+            //     divvy2.innerHTML += "<br>";
+            // }
             // divvy.innerHTML.replace(" ", ".");
             // divvy2.innerHTML.replace(" ", ".");
             do_stuff(floor1, pic1);
