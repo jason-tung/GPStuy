6a464f495c41cdf340baab231686f1d2d3041131
Jeffrey Wu <jefffreywu@gmail.com>
2019-06-05 21:33:25 -0400

    added table to home page

--- static/js/runmin.js
+++ static/js/runmin.js
@@ -152,7 +152,14 @@ function find(a, b) {
             do_stuff(floor1, pic1);
             do_stuff(floor2, pic2);
             if (same_floor){
-                do_stuff(floor1, pic1);
+                var data = gendirections(floor1, false);
+                createTable(data);
+            }
+            else{
+                var data1 = gendirections(floor1,false,t2.substring(0,t2.length-2));
+                var data2 = gendirections(floor2,true);
+                createTable(data1.concat(data2));
+    
             }
 
         },
@@ -162,3 +169,125 @@ function find(a, b) {
         }
     );
 };
+
+function gendirections(map,stairs1,floor) {
+    var instruct;
+    if (stairs1)
+        instruct = ['Exit the stairs'];
+    else 
+        instruct = ['Exit the room'];
+    console.log("hi")
+    var curX;
+    var curY; 
+    var curDir;
+    var steps =0; 
+
+    for (var i = 0; i < map.length; i++)
+    {
+        for (var j = 0; j < map[0].length; j++)
+        {
+            if (map[i][j] == 'S')
+            {
+                curX = i;
+                curY = j ;
+            }
+        }
+    }
+    console.log(curX,curY);
+   
+    var directions = [[0,1],[-1,0],[0,-1],[1,0]];
+    for(var i = 0; i< 4; i++){
+        var nextX = curX + directions[i][0]
+        var nextY = curY + directions[i][1]
+        console.log(nextX,nextY)
+        if (map[nextX][nextY] == '@'){
+            curDir = i;
+            curX = nextX;
+            curY = nextY;
+            steps++;
+        }
+    }
+    console.log(curDir);
+    while(map[curX+directions[curDir][0]][curY+directions[curDir][1]] != 'E' ){
+        while (map[curX+directions[curDir][0]][curY+directions[curDir][1]] == "@")
+        {
+            console.log('while loop started')
+            // for(var i = 0; i <4;i++){
+            console.log(map[curX+directions[curDir][0]][curY+directions[curDir][1]]);
+            console.log(map[curX+directions[curDir][0]][curY+directions[curDir][1]] == "@");
+            curX += directions[curDir][0];
+            curY += directions[curDir][1];
+            console.log(curX,curY);
+            steps++; 
+        } 
+        
+        console.log('hi')
+        instruct.push("Move forward " +steps+ " ft.");
+        console.log(instruct);
+        console.log(steps);
+        steps = 0;
+
+        d = directions[(curDir+1)%4 ];
+        if(map[curX+d[0]][curY+d[1]] == '@' || map[curX+d[0]][curY+d[1]] == 'E'){
+            instruct.push('Turn Left')
+            curDir++;
+            curDir %= 4;
+        }
+        else{
+            instruct.push('Turn Right')
+            if (curDir == 0){
+                curDir = 3;
+            }
+            else{
+                curDir--;
+            }
+        }
+        
+        for(var i = 0; i< 4; i++){
+            var nextX = curX + directions[i][0];
+            var nextY = curY + directions[i][1];
+            if (map[nextX][nextY] == 'E'){
+                console.log('whats up');
+                curDir = i;
+            }
+        }
+    }
+    if(floor){
+        instruct.push('Take the stairs to Floor ' + floor);
+    }
+    else{
+        instruct.push('You have arrived at your destination.');
+        }
+
+    console.log(instruct)
+    return instruct;
+};
+
+function createTable(dt) {
+    var table = document.createElement('table');
+    var tableBody = document.createElement('tbody');
+    var rhead = document.createElement('tr');
+    var ind = document.createElement('th');
+    ind.appendChild(document.createTextNode('Step #'));
+    var dir = document.createElement('th');
+    dir.appendChild(document.createTextNode('Direction'));
+    rhead.appendChild(ind);
+    rhead.appendChild(dir);
+    tableBody.appendChild(rhead);
+
+    for(var i = 0; i < dt.length; i++){
+        var row = document.createElement('tr');
+        var ind = document.createElement('td');
+        ind.appendChild(document.createTextNode(i+1));
+        row.appendChild(ind);
+        var dir = document.createElement('td');
+        dir.appendChild(document.createTextNode(dt[i]));
+        row.appendChild(dir);
+        tableBody.appendChild(row);
+
+    }
+    
+    table.appendChild(tableBody);
+    table.setAttribute("class", "table");
+    document.body.appendChild(table);
+  }
