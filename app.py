a8fafd8dbfb2faf9d9804608531430b0458591c0
Jason Tung <mickeu420@gmail.com>
2019-06-07 10:37:48 -0400

    saved...

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ app.secret_key = os.urandom(32)  # key for session
 #     print("MEGA FAIL")
 #     db_create.setup()
 
+
+schedule.create_stuy_schedule()
+
+db_create.setup()
+
+
 @app.route('/')
 def home():
     try:
@@ -29,7 +35,8 @@ def home():
         current_p = schedule.get_current_period(type if type else "REGULAR")
         if session['id'] == db_create.getIDFromEmail("jwu29@stuy.edu"):
             return redirect(url_for("admin_features"))
-        return render_template("home.html", name = db_create.get_user_by_id(session['id']), periods = db_create.get_periods_from_id(session['id']), current_period = current_p)
+        return render_template("home.html", name=db_create.get_user_by_id(session['id']),
+                               periods=db_create.get_periods_from_id(session['id']), current_period=current_p)
     except KeyError:
         return render_template("home.html")
 
@@ -39,19 +46,22 @@ def api_path():
     a = request.args['loc1']
     b = request.args['loc2']
     pairpath = ms.path(a, b)
-    #print(pairpath)
+    # print(pairpath)
     try:
         return dumps(pairpath)
     except:
         return "poopy"
 
+
 @app.route('/test_api', methods=["GET", "POST"])
 def test_api():
     return render_template("api_test.html")
 
+
 @app.route('/register')
 def register():
     return render_template("register.html")
+
 
 @app.route('/signup', methods=["GET", "POST"])
 def sign_up():
@@ -65,7 +75,8 @@ def sign_up():
             db_create.add_user(name, email, guardian_email, pw)
             session['id'] = db_create.getIDFromEmail(email)
             flash('Account successfully created!')
-            return render_template("home.html", name = db_create.get_user_by_id(session['id']), periods = db_create.get_periods_from_id(session['id']))
+            return render_template("home.html", name=db_create.get_user_by_id(session['id']),
+                                   periods=db_create.get_periods_from_id(session['id']))
 
         except IntegrityError:
             flash('Email already in use. Please try again.')
@@ -75,9 +86,11 @@ def sign_up():
         flash('Passwords do not match!')
         return redirect(url_for('register'))
 
+
 @app.route('/login')
 def login():
     return render_template("login.html")
+
 
 @app.route('/logout')
 def logout():
@@ -94,7 +107,7 @@ def logout():
 def auth():
     email = request.form.get("email")
     pw = request.form.get("pw")
-    succ = db_create.authenticate(email,pw)
+    succ = db_create.authenticate(email, pw)
     if succ:
         session['id'] = db_create.getIDFromEmail(email)
         flash("Successfully logged in.")
@@ -102,29 +115,32 @@ def auth():
         current_p = schedule.get_current_period(type if type else "REGULAR")
         if (email == "jwu29@stuy.edu"):
             return redirect(url_for("admin_features"))
-        return render_template("home.html", name = db_create.get_user_by_id(session['id']), periods = db_create.get_periods_from_id(session['id']), current_period = current_p)
+        return render_template("home.html", name=db_create.get_user_by_id(session['id']),
+                               periods=db_create.get_periods_from_id(session['id']), current_period=current_p)
     else:
         flash("Password is incorrect.")
         return redirect(url_for('login'))
+
 
 @app.route('/profile')
 def profile():
     try:
         student_periods = db_create.get_periods_from_id(session['id'])
-        return render_template("profile.html", name = db_create.get_user_by_id(session['id']), periods = student_periods)
+        return render_template("profile.html", name=db_create.get_user_by_id(session['id']), periods=student_periods)
 
     except KeyError:
         flash("You are not logged in.")
         return redirect(url_for("home"))
 
+
 @app.route('/schedule_updater', methods=["GET", "POST"])
 def schedule_updater():
-
     try:
         id = session['id']
         _name = db_create.get_user_by_id(id)
 
-        _periods = ["period1", "period2", "period3", "period4", "period5", "period6", "period7", "period8", "period9", "period10"]
+        _periods = ["period1", "period2", "period3", "period4", "period5", "period6", "period7", "period8", "period9",
+                    "period10"]
 
         for period in _periods:
             room = request.form.get(period)
@@ -137,7 +153,6 @@ def schedule_updater():
                     room = "'" + room + "'"
                 db_create.insert_room(id, period, room)
 
-
         student_periods = db_create.get_periods_from_id(id)
         flash("Successfully updated rooms.")
         # return render_template("profile.html", name = _name, periods = student_periods)
@@ -145,6 +160,7 @@ def schedule_updater():
     except KeyError:
         flash("You are not logged in.")
         return redirect(url_for("home"))
+
 
 @app.route('/bell_schedule', methods=["GET", "POST"])
 def bell_schedule():
@@ -155,51 +171,61 @@ def bell_schedule():
     converted_periods = []
     current_p = schedule.get_current_period(choice)
 
-    start_minute = str(list_periods[-1][1].minute) if list_periods[-1][1].minute >= 10 else '0' + str(list_periods[-1][1].minute)
-    end_minute = str(list_periods[0][0].minute) if list_periods[0][0].minute >= 10 else '0' + str(list_periods[0][0].minute)
-    start = str(list_periods[-1][1].hour) + ":" + start_minute + " AM" if list_periods[-1][1].hour/12 <= 1 else str(list_periods[-1][1].hour%12) + ":" + start_minute + " PM"
-    end = str(list_periods[0][0].hour) + ":" + end_minute + " AM" if list_periods[0][0].hour/12 <= 1 else str(list_periods[0][0].hour%12) + ":" + end_minute + " PM"
-    after_before_school = (start,end)
+    start_minute = str(list_periods[-1][1].minute) if list_periods[-1][1].minute >= 10 else '0' + str(
+        list_periods[-1][1].minute)
+    end_minute = str(list_periods[0][0].minute) if list_periods[0][0].minute >= 10 else '0' + str(
+        list_periods[0][0].minute)
+    start = str(list_periods[-1][1].hour) + ":" + start_minute + " AM" if list_periods[-1][1].hour / 12 <= 1 else str(
+        list_periods[-1][1].hour % 12) + ":" + start_minute + " PM"
+    end = str(list_periods[0][0].hour) + ":" + end_minute + " AM" if list_periods[0][0].hour / 12 <= 1 else str(
+        list_periods[0][0].hour % 12) + ":" + end_minute + " PM"
+    after_before_school = (start, end)
 
     t_day = datetime.now()
-    t_day = schedule.get_current_weekday() + ", " + schedule.get_current_month() + " " + str(t_day.day) + ", " + str(t_day.year)
-    day_type = schedule.get_bell_schedule() # in the form CONFERENCE/HOMEROOM/REGULAR, A, B
+    t_day = schedule.get_current_weekday() + ", " + schedule.get_current_month() + " " + str(t_day.day) + ", " + str(
+        t_day.year)
+    day_type = schedule.get_bell_schedule()  # in the form CONFERENCE/HOMEROOM/REGULAR, A, B
     day_type = day_type + ", " + schedule.get_type_of_day() if day_type and schedule.get_type_of_day() else day_type
     for p in list_periods:
         start_minute = str(p[0].minute) if p[0].minute >= 10 else '0' + str(p[0].minute)
         end_minute = str(p[1].minute) if p[1].minute >= 10 else '0' + str(p[1].minute)
-        start = str(p[0].hour) + ":" + start_minute + " AM" if p[0].hour/12 <= 1 else str(p[0].hour%12) + ":" + start_minute + " PM"
-        end = str(p[1].hour) + ":" + end_minute + " AM" if p[1].hour/12 <= 1 else str(p[1].hour%12) + ":" + end_minute + " PM"
+        start = str(p[0].hour) + ":" + start_minute + " AM" if p[0].hour / 12 <= 1 else str(
+            p[0].hour % 12) + ":" + start_minute + " PM"
+        end = str(p[1].hour) + ":" + end_minute + " AM" if p[1].hour / 12 <= 1 else str(
+            p[1].hour % 12) + ":" + end_minute + " PM"
 
         converted_periods.append((start, end))
     try:
-        return render_template("bell_schedule.html", name = db_create.get_user_by_id(session['id']), option = choice, periods = converted_periods, current_period = current_p, buffer = after_before_school, today = t_day, day_info = day_type)
+        return render_template("bell_schedule.html", name=db_create.get_user_by_id(session['id']), option=choice,
+                               periods=converted_periods, current_period=current_p, buffer=after_before_school,
+                               today=t_day, day_info=day_type)
 
     except KeyError:
-        return render_template("bell_schedule.html", option = choice, periods = converted_periods, current_period = current_p, buffer = after_before_school, today = t_day, day_info = day_type)
+        return render_template("bell_schedule.html", option=choice, periods=converted_periods, current_period=current_p,
+                               buffer=after_before_school, today=t_day, day_info=day_type)
 
-#Admin Stuff
 
-@app.route('/admin_features', methods=["GET","POST"])
-def admin_features(): #Send mass emails, upload layouts of schools, see list of students, edit students schedules (i.e. change room number and teacher name)
+# Admin Stuff
+
+@app.route('/admin_features', methods=["GET", "POST"])
+def admin_features():  # Send mass emails, upload layouts of schools, see list of students, edit students schedules (i.e. change room number and teacher name)
     print(db_create.get_user_by_id(session['id']))
     if db_create.get_user_by_id(session['id'])[0] == 'admin':
-        is_post = request.method == "POST" 
+        is_post = request.method == "POST"
         if is_post:
             if request.form['action'] == "Display":
                 table = db_create.get_email_list()
                 print(table)
-                return render_template("admin.html", t = table)
+                return render_template("admin.html", t=table)
             elif request.form['action'] == "Search":
                 table = db_create.search(request.form['stud_name'])
-                return render_template("admin.html", t = table)
-            return render_tempalte("admin.html")
-        else: 
+                return render_template("admin.html", t=table)
+            return render_template("admin.html")
+        else:
             return render_template("admin.html")
     return redirect(url_for("home"))
 
+
 if __name__ == "__main__":
-    db_create.setup()
-    app.debug = True
-    schedule.create_stuy_schedule()
+    # app.debug = True
     app.run()
